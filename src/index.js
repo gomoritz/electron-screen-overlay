d4516ef14e4966c85ab1f72d542cd0f58ccf60ce
flaaaps <julianwening4@gmail.com>
2021-03-17 00:04:55 +0100

    :sparkles: Improve deactivation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
-const electron = require("electron")
-const { app, BrowserWindow } = electron
+const electron = require('electron');
+const { app, BrowserWindow, ipcMain } = electron;
+const path = require('path');
 
 let window;
 let screen;
 
-app.on("ready", () => setTimeout(() => {
-    screen = electron.screen.getPrimaryDisplay()
-    window = new BrowserWindow({
-        width: screen.size.width,
-        height: screen.size.height,
-        transparent: true,
-        frame: false,
-        alwaysOnTop: true,
-        show: false
-    })
-    window.setIgnoreMouseEvents(true)
-    window.setFocusable(false)
+app.on(
+	'ready',
+	() =>
+		setTimeout(() => {
+			const NODE_ENV = process.env.NODE_ENV;
+			screen = electron.screen.getPrimaryDisplay();
+			console.log(screen.size.width);
+			window = new BrowserWindow({
+				// width: screen.size.width,
+				// height: screen.size.height,
+				width: screen.size.width,
+				height: screen.size.height,
+				webPreferences: {
+					devTools: true,
+					nodeIntegration: true,
+					contextIsolation: false
+				},
+				transparent: true,
+				frame: false,
+				alwaysOnTop: NODE_ENV === 'development' ? false : true,
+				show: false
+			});
+			if (NODE_ENV !== 'development') {
+				// window.setIgnoreMouseEvents(true);
+				window.setFocusable(false);
+			}
 
-    window.loadFile("src/index.html")
-        .then(() => window.show())
-        .catch(console.error)
-}, 0))
+			window
+				.loadFile(path.resolve(__dirname, 'index.html'))
+				.then(() => window.show())
+				.catch(console.error);
+		}),
+	0
+);
 
+ipcMain.on('set-ignore-mouse-events', (event, ...args) => {
+	BrowserWindow.fromWebContents(event.sender).setIgnoreMouseEvents(...args);
+});
+
+ipcMain.on('disable-confirm', () => {
+	console.log('Disable received!');
+	window.close();
+});
+
+// trigger termination in main process
 setTimeout(() => {
-    window.close()
-}, 10_000)
+	window.webContents.send('disable');
+}, 30_000);
