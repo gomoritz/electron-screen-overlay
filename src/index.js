5e32f18d3ef889f9f7f650e88831c29c4c7fd8aa
moritz <theincxption@gmail.com>
2021-03-17 17:34:55 +0100

    :lipstick: Create more animations and rework window behavior

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-const electron = require("electron");
-const { app, BrowserWindow, ipcMain } = electron;
-const path = require("path");
+const electron = require("electron")
+const { app, BrowserWindow, ipcMain } = electron
+const path = require("path")
 
-let window;
-let screen;
+let window
+let screen
 
 app.on(
     "ready",
     () =>
         setTimeout(() => {
-            const NODE_ENV = process.env.NODE_ENV;
-            screen = electron.screen.getPrimaryDisplay();
-            console.log(screen.size.width);
-            console.log(screen.size.height);
+            screen = electron.screen.getPrimaryDisplay()
+            console.log(screen.size.width)
+            console.log(screen.size.height)
             window = new BrowserWindow({
-                // width: screen.size.width,
-                // height: screen.size.height,
                 x: 0,
-                y: screen.size.height / 2 - 100,
+                y: screen.size.height - 250,
                 width: screen.size.width,
+                height: 250,
                 webPreferences: {
                     devTools: true,
                     nodeIntegration: true,
@@ -26,33 +24,33 @@ app.on(
                 },
                 transparent: true,
                 frame: false,
-                alwaysOnTop: NODE_ENV === "development" ? false : true,
+                alwaysOnTop: true,
                 show: false,
-                skipTaskbar: true,
-            });
-            if (NODE_ENV !== "development") {
-                window.setIgnoreMouseEvents(false);
-                window.setFocusable(false);
-            }
+            })
+
+            window.setIgnoreMouseEvents(false)
+            window.setFocusable(false)
+            // window.webContents.toggleDevTools()
 
             window
                 .loadFile(path.resolve(__dirname, "index.html"))
                 .then(() => window.show())
-                .catch(console.error);
+                .catch(console.error)
         }),
     0
-);
+)
 
 ipcMain.on("set-ignore-mouse-events", (event, ...args) => {
-    BrowserWindow.fromWebContents(event.sender).setIgnoreMouseEvents(...args);
-});
+    BrowserWindow.fromWebContents(event.sender).setIgnoreMouseEvents(...args)
+})
 
 ipcMain.on("disable-confirm", () => {
-    console.log("Disable received!");
-    window.close();
-});
+    console.log("Disable received!")
+    window.close()
+})
 
 // trigger termination in main process
 setTimeout(() => {
-    window.webContents.send("disable");
-}, 30_000);
+    console.log("Timing out...")
+    window.webContents.send("disable")
+}, 5_000)
