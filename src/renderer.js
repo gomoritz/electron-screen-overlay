8185c83afb2910710e98eeb077834fde5e3f1633
moritz <theincxption@gmail.com>
2021-03-17 17:35:13 +0100

    :lipstick: Create more animations and rework window behavior

--- src/renderer.js
+++ src/renderer.js
@@ -1,33 +1,31 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer } = require('electron')
 
-// setTimeout(() => {}, 7_000);
-
-const main = document.getElementById('main');
+const main = document.querySelector('.main')
 ipcRenderer.on('disable', () => {
-	console.log('Disable received');
-	disable();
-});
+    console.log('Disable received')
+    disable()
+})
 
-const innerCircle = document.getElementById('inner-circle');
+const circle = document.querySelector('.circle')
+const innerCircle = document.querySelector('.inner-circle')
 
 // trigger termination from window
-innerCircle.addEventListener('click', () => disable());
+circle.addEventListener('click', () => disable())
 
 // not the best solution, but works!
 window.addEventListener('mousemove', e => {
-	if (e.target === innerCircle) {
-		console.log('Inner circle is the target!!');
-		ipcRenderer.send('set-ignore-mouse-events', false, { forward: true });
-	} else {
-		ipcRenderer.send('set-ignore-mouse-events', true, { forward: true });
-	}
-});
+    if (e.target === innerCircle) {
+        console.log('Inner circle is the target!!')
+        ipcRenderer.send('set-ignore-mouse-events', false, { forward: true })
+    } else {
+        ipcRenderer.send('set-ignore-mouse-events', true, { forward: true })
+    }
+})
 
 function disable() {
-	main.classList.add('fade-out');
-	const listener = main.addEventListener('animationend', () => {
-		console.log('Animation-end triggered.');
-		ipcRenderer.send('disable-confirm');
-	});
-	main.removeEventListener('animationend', listener);
+    main.classList.add('dismiss')
+    circle.classList.add("dismiss-circle")
+    innerCircle.classList.add("dismiss-inner-circle")
+
+    setTimeout(() => ipcRenderer.send('disable-confirm'), 2000)
 }
